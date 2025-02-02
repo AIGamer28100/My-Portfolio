a0d23925ef929ac88ec1ebf4ba07cb4d71fc4a75
Ubuntu <ubuntu@ip-172-31-84-51.ec2.internal>
2025-02-02 13:09:21 +0000

    Server Code

--- mysite/urls.py
+++ mysite/urls.py
@@ -31,5 +31,8 @@ urlpatterns = [
     path('hmail/', include('mail.urls', namespace='mail')),
     path('todo/', include('todo.urls', namespace='todo')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
